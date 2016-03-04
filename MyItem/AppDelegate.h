337206b7cea398efff23cbc9c2bcbb6bcb6fef87
RuoLiWuShengJie <469912605@qq.com@wushengjie.local>
2016-03-04 09:56:51 +0800

    Initial Commit

---
+++ MyItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyItem
+//
+//  Created by 吴胜杰 on 16/3/4.
+//  Copyright © 2016年 wushengjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyItem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyItem
+//
+//  Created by 吴胜杰 on 16/3/4.
+//  Copyright © 2016年 wushengjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
